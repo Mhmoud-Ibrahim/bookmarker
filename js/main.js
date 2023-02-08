a79af9ca3e5c4d692f8fd401f9651a711e73d44f
Mahmoud Ibrahim <mahmoudd188@gmail.com>
2023-02-08 13:07:02 +0200

    update project

--- js/main.js
+++ js/main.js
@@ -2,29 +2,41 @@
 var siteNameInput = document.getElementById('siteName');
 var siteUrlINput = document.getElementById('siteUrl');
 var submitBtn = document.getElementById('submitBtn');
-var deleteBtn = document.getElementById('deleteBtn')
-
+var deleteBtn = document.getElementById('deleteBtn');
+var searchInput = document.getElementById('search');
 var bookmarksContainer = [];
 if(localStorage.getItem('bookmark') != null){ // check localstorage
   bookmarksContainer=JSON.parse(localStorage.getItem('bookmark')) 
   displayMarks(bookmarksContainer)
 }
+   //validate functions//
+function validateName(){
+  var regex = /^[A-z][a-z]{3,10}$/ 
+ return regex.test(siteNameInput.value);
+}
+function validateUrl(){
+  var regex = /^(ftp|http|https):\/\/[^ "]+$/ 
+ return regex.test(siteUrlINput.value);
+}
+
  function addBookMark(){ //add function
-
-  if(validateBookmarkName() == true){
-
+  if((validateName() && validateUrl()) == true){
     var  mark= {
-         siteName:siteNameInput.value,
-         siteUrl:siteUrlINput.value,
-     }
-     bookmarksContainer.push(mark);
-     localStorage.setItem('bookmark',JSON.stringify(bookmarksContainer))
-     displayMarks()
-     clearFrom()
-    }else{
-      // alert('name is required')
-       document.getElementById('alert').classList.replace('d-none','d-inline-block')
-    }
+      siteName:siteNameInput.value,
+      siteUrl:siteUrlINput.value,
+      }
+      bookmarksContainer.push(mark);
+      localStorage.setItem('bookmark',JSON.stringify(bookmarksContainer))
+      displayMarks()
+      clearFrom()
+      document.getElementById('alert').classList.replace('d-inline-block','d-none')
+      document.getElementById('alert2').classList.replace('d-inline-block','d-none')
+
+    }else if(validateName() == false){
+        document.getElementById('alert').classList.replace('d-none','d-inline-block')
+    }else if ( validateUrl() == false){
+          document.getElementById('alert2').classList.replace('d-none','d-inline-block')
+        }
   }
 submitBtn.addEventListener('click',function(){
   addBookMark()
@@ -43,27 +55,32 @@ function displayMarks(){ //display function
   }
   document.getElementById('bookmarklist').innerHTML= marks;
 }
-
-function clearFrom(){  //clear form
+function clearFrom(){  //clear function
         siteNameInput.value="",
         siteUrlINput.value = ""
 }
-
 function deletebBookMark(index){ //delet function
   bookmarksContainer.splice(index,1);
   localStorage.setItem('bookmark',JSON.stringify(bookmarksContainer))
     displayMarks()
-
 }
-// deleteBtn.addEventListener('click',function(){
-//   deletebBookMark(i);
-
-// })
-function validateBookmarkName(){
-  var regex = /^[A-z][a-z]{3,10}$/ 
- return regex.test(siteNameInput.value)
-}
-// function validateBookmarkName(){
-//   var regex = /^https://www{1}.$/ 
-//  return regex.test(siteUrlINput.value)
+// function search(term){
+//   var searched=``;
+//   for(var i=0;i<bookmarksContainer.length;i++){
+//     if(bookmarksContainer[i].siteName.toLawerCase().includes(term.toLawerCase())){
+//       searched += `
+//       <div class="webwell row mt-4 ">
+//       <h2 class="d-inline">${bookmarksContainer[i].siteName.replace(term,'<span>' + term+ '</span> '   )}</h2>
+//       <a class="btn btn-primary m-1 "  href="${bookmarksContainer[i].siteUrl}">visit</a>
+//       <button id="deleteBtn" class="btn btn-danger  m-1 " onclick="deletebBookMark(${i})" >delete</button>
+//       </div>
+//       `
+//     }
+//   }
+//   document.getElementById('bookmarklist').innerHTML= searched;
+//   ;
 // }
+
+// searchInput.addEventListener("oninput", function(){
+//   search(term);
+// })
